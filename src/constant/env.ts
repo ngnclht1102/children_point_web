c1235735536e77e997c0d80714c8e19c81ecfdf2
brian.nguyen <ngnclht@gmail.com>
2026-01-17 10:16:41 +0700

    fix(env): update api url

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -3,6 +3,6 @@ export const isLocal = process.env.NODE_ENV === 'development';
 
 console.log('isProd', isProd);
 export const API_URL = isLocal
-  ? 'http://localhost:8081'
-  : 'https://api.tichdiemhocsinh.com';
+  ? 'http://localhost:1968'
+  : 'https://service.tichdiemhocsinh.com';
 export const showLogger = true;
